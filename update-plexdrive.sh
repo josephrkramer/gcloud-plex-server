e2f30c055cc323203174427183e4b93612cf890c
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2022-02-06 19:27:40 +0000

    Add restart and success message

--- update-plexdrive.sh
+++ update-plexdrive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/plexdrive/plexdrive/releases/latest/download/plexdrive-linux-amd64 && chmod +x plexdrive-linux-amd64 && sudo mv plexdrive-linux-amd64 /usr/bin/plexdrive
+wget https://github.com/plexdrive/plexdrive/releases/latest/download/plexdrive-linux-amd64 && chmod +x plexdrive-linux-amd64 && sudo mv plexdrive-linux-amd64 /usr/bin/plexdrive && sudo systemctl restart plexdrive.service && echo "Plexdrive successfully updated."
