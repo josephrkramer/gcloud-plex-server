50787928ff986b01384de1b53bed255e66fd8dc2
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2022-09-25 14:34:46 -0400

    fix adding plex repo

--- install-plex.sh
+++ install-plex.sh
@@ -3,7 +3,7 @@
 sudo apt install curl
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 
-$ echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 
 sudo apt install apt-transport-https
 sudo apt update
