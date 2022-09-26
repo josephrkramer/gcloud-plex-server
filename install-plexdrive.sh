0ba26521b16ce0d7e44f4bea6416007fae5b212d
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2022-09-26 17:41:49 -0400

    Update install-plexdrive.sh

--- install-plexdrive.sh
+++ install-plexdrive.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget https://github.com/plexdrive/plexdrive/releases/latest/download/plexdrive-linux-amd64 && chmod +x plexdrive-linux-amd64 && sudo mv plexdrive-linux-amd64 /usr/bin/plexdrive && sudo cp plexdrive.service /etc/systemd/system/plexdrive.service && sudo plexdrive mount -c /root/.plexdrive -o allow_other /mnt/plexdrive
+wget https://github.com/plexdrive/plexdrive/releases/latest/download/plexdrive-linux-amd64 && chmod +x plexdrive-linux-amd64 && sudo mv plexdrive-linux-amd64 /usr/bin/plexdrive && sudo cp plexdrive.service /etc/systemd/system/plexdrive.service && sudo systemctl start plexdrive.service && sudo systemctl enable plexdrive.service
+#&& sudo plexdrive mount -c /root/.plexdrive -o allow_other /mnt/plexdrive
