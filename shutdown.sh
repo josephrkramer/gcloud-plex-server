c3626a3723fd11bd7f5728f747ec27ab888a367b
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-02-18 19:15:35 +0000

    combine and clean up

--- shutdown.sh
+++ shutdown.sh
@@ -46,6 +46,38 @@ gcloud compute snapshots create $SNAPSHOT_NAME \
      --source-disk-zone=$ZONE \
      --storage-location=us-east1
 
+#-------------------------------------------
+
+# Get a list of snapshots with creation timestamps, sorted in ascending order (oldest first)
+snapshots=$(gcloud compute snapshots list \
+            --format="value(name,creationTimestamp)" \
+            --sort-by=creationTimestamp)
+
+# Count the number of snapshots
+snapshot_count=$(echo "$snapshots" | wc -l)
+echo "Number of snapshots: $snapshot_count"
+
+# Extract the name of the oldest snapshot (the first line in the output)
+oldest_snapshot=$(echo "$snapshots" | head -n 1 | awk '{print $1}')
+
+# Check if a snapshot was found
+if [ -z "$oldest_snapshot" ]; then
+  echo "No snapshots found"
+else
+  echo "Oldest snapshot: $oldest_snapshot"
+fi
+
+#Delete the instance
+if (( snapshot_count > 2 )); then
+    echo "Found $snapshot_count snapshots, deleting the oldest snapshot: $oldest_snapshot"
+    gcloud compute snapshots delete $oldest_snapshot
+else
+    echo "Only $snapshot_count snapshot(s), not deleting anything"
+fi
+
+#-------------------------------------------
+
+gcloud compute instances delete $most_recent_vm --zone $ZONE
 
 echo "Done. Plex server shutdown."
 
