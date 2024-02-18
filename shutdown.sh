ffe9923307407202f95f84115edaea78d8cae054
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-02-18 14:27:00 -0500

    Automated scripts (#1)
    * initial startup script
    
    * all in one command
    
    * initial shutdown script
    
    * simplify commands
    
    * two step creation
    
    * fix snapshotting
    
    * improve logging
    
    * wip delete
    
    * delete snapshot
    
    * combine and clean up
    
    * improved logging

---
+++ shutdown.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+ZONE=us-east1-b
+INIT_DATE=$(date +%Y%m%d-%H%M%S)
+SNAPSHOT_NAME=plex-snapshot-$INIT_DATE
+
+# Get a list of VM instances with creation timestamps, sorted in descending order
+instances=$(gcloud compute instances list \
+            --format="value(name,creationTimestamp)" \
+            --sort-by=~creationTimestamp)
+
+# Extract the name of the most recent VM (the first line in the output)
+most_recent_vm=$(echo "$instances" | head -n 1 | awk '{print $1}')
+
+# Check if a VM was found
+if [ -z "$most_recent_vm" ]; then
+  echo "No VM instances found"
+  exit 1
+else
+  echo "Most recent VM: $most_recent_vm"
+fi
+
+echo "Stopping instance $most_recent_vm"
+gcloud compute instances stop $most_recent_vm --zone $ZONE
+
+# Get a list of disks with creation timestamps, sorted in descending order
+disks=$(gcloud compute disks list \
+            --format="value(name,creationTimestamp)" \
+            --sort-by=~creationTimestamp)
+
+# Extract the name of the most recent VM (the first line in the output)
+most_recent_disk=$(echo "$disks" | head -n 1 | awk '{print $1}')
+
+# Check if a VM was found
+if [ -z "$most_recent_disk" ]; then
+  echo "No disks found"
+  exit 1
+else
+  echo "Most recent disk: $most_recent_disk"
+fi
+
+#gcloud compute snapshots create snapshot-1 --project=basic-lock-251300 --source-disk=plex-server-20240218-181437 --source-disk-zone=us-east1-b --storage-location=us-east1
+echo "Creating snapshot $SNAPSHOT_NAME from disk $most_recent_disk"
+gcloud compute snapshots create $SNAPSHOT_NAME \
+     --source-disk=$most_recent_disk \
+     --source-disk-zone=$ZONE \
+     --storage-location=us-east1
+
+#-------------------------------------------
+
+# Get a list of snapshots with creation timestamps, sorted in ascending order (oldest first)
+snapshots=$(gcloud compute snapshots list \
+            --format="value(name,creationTimestamp)" \
+            --sort-by=creationTimestamp)
+
+# Count the number of snapshots
+snapshot_count=$(echo "$snapshots" | wc -l)
+echo "Number of snapshots: $snapshot_count"
+
+# Extract the name of the oldest snapshot (the first line in the output)
+oldest_snapshot=$(echo "$snapshots" | head -n 1 | awk '{print $1}')
+
+# Check if a snapshot was found
+if [ -z "$oldest_snapshot" ]; then
+  echo "No snapshots found, leaving VM instance in tact as a snapshot should have been created"
+  exit 1
+else
+  echo "Oldest snapshot: $oldest_snapshot"
+fi
+
+#Delete the instance
+if (( snapshot_count > 2 )); then
+    echo "Found $snapshot_count snapshots, deleting the oldest snapshot: $oldest_snapshot"
+    gcloud compute snapshots delete $oldest_snapshot
+else
+    echo "Only $snapshot_count snapshot(s), not deleting anything"
+fi
+
+#-------------------------------------------
+
+gcloud compute instances delete $most_recent_vm --zone $ZONE
+
+echo "Done. Plex server shutdown."
+
---
+++ startup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ZONE=us-east1-b
+TEMPLATE=plex-template-1
+INIT_DATE=$(date +%Y%m%d-%H%M%S)
+DISK_NAME=plex-disk-$INIT_DATE
+SERVER_NAME=plex-server-$INIT_DATE
+
+# Get a list of snapshots, along with their creation timestamps, sorted in descending order 
+snapshots=$(gcloud compute snapshots list \
+            --format="value(name,creationTimestamp)" \
+            --sort-by=~creationTimestamp)
+
+# Extract the name of the most recent snapshot (the first line in the output)
+most_recent_snapshot=$(echo "$snapshots" | head -n 1 | awk '{print $1}')
+
+# Check if a snapshot was found
+if [ -z "$most_recent_snapshot" ]; then
+  echo "No snapshots found"
+  exit 1
+else
+  echo "Most recent snapshot: $most_recent_snapshot"
+fi
+
+#create disk from snapshot
+echo "Creating disk $DISK_NAME from $most_recent_snapshot"
+gcloud compute disks create $DISK_NAME \
+    --source-snapshot=$most_recent_snapshot \
+    --zone=$ZONE
+
+#create instance using template and disk
+echo "Creating instance $SERVER_NAME from $DISK_NAME"
+gcloud compute instances create $SERVER_NAME \
+  --zone=$ZONE \
+  --source-instance-template=$TEMPLATE \
+  --disk=name=$DISK_NAME,boot=yes,auto-delete=yes
+
+echo "Done. Plex server ready."
+
